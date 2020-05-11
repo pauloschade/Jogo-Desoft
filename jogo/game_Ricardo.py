cf5f544869e3fa486dd322cb1171e80353a847ba
pauloschade <paulosc4@al.insper.edu.br>
2020-05-11 11:53:25 -0300

    jogo paulo
    copiei toshi

--- jogo/game_Ricardo.py
+++ jogo/game_Ricardo.py
@@ -1,5 +1,9 @@
 import pygame
+import random
+from os import path
 
+
+imagens = path.join(path.dirname(__file__), "assets/img")
 pygame.init()
 
 dimns = (1200, 410)
@@ -10,7 +14,7 @@ font = pygame.font.SysFont(None, 48)
 pygame.display.set_caption('Jogo do Mat')
 earth_land = pygame.image.load('assets/img/earth_land.png').convert()
 background1 = pygame.transform.scale(earth_land,dimns)
-player_img = pygame.image.load('assets/img/darth_maul.png').convert_alpha()
+player_img = pygame.image.load(path.join(imagens, 'wakanda.png')).convert_alpha()
 player_img_small =  pygame.transform.scale(player_img, (player_WIDTH, player_HEIGHT))
 player_x = 600
 player_y = 270
