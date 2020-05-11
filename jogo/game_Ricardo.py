9ce571c23c943d9cd0e254235862afa1a1f76081
luizrhpf100 <62723614+luizrhpf100@users.noreply.github.com>
2020-05-11 11:24:39 -0300

    player

---
+++ jogo/game_Ricardo.py
@@ -0,0 +1,33 @@
+import pygame
+
+pygame.init()
+
+dimns = (1200, 410)
+window = pygame.display.set_mode(dimns)
+player_HEIGHT = 81
+player_WIDTH = 81
+font = pygame.font.SysFont(None, 48)
+pygame.display.set_caption('Jogo do Mat')
+earth_land = pygame.image.load('assets/img/earth_land.png').convert()
+background1 = pygame.transform.scale(earth_land,dimns)
+player_img = pygame.image.load('assets/img/darth_maul.png').convert_alpha()
+player_img_small =  pygame.transform.scale(player_img, (player_WIDTH, player_HEIGHT))
+player_x = 600
+player_y = 270
+
+game = True
+
+while game:
+    for event in pygame.event.get():
+        if event.type == pygame.KEYUP:
+            game = False
+
+    window.fill((0, 0, 0))
+    window.blit(background1, (0, 0))
+    window.blit(player_img_small, (player_x, player_y))
+
+    pygame.display.update()
+
+pygame.quit()
+
+
